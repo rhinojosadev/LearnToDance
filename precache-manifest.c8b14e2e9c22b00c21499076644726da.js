7cb51364b00bc88e85193f74f8bbcf794f12c629
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-05 07:17:06 +0000

    Deploy rene-ph/LearnToDance to github.com/rene-ph/LearnToDance.git:gh-pages

---
+++ precache-manifest.c8b14e2e9c22b00c21499076644726da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93951ed10f9108f52b0ec74e1517515c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8337260d20b96a86d5b2",
+    "url": "./static/css/main.5feddfe1.chunk.css"
+  },
+  {
+    "revision": "32f31f7513062f828847",
+    "url": "./static/js/2.0a9be871.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.0a9be871.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8337260d20b96a86d5b2",
+    "url": "./static/js/main.50f51bf3.chunk.js"
+  },
+  {
+    "revision": "23dd6d3316230d669d12",
+    "url": "./static/js/runtime-main.ea2ad56e.js"
+  }
+]);
